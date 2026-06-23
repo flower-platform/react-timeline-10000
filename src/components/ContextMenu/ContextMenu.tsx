fdf379af7e8ac7be0948373aaad996b604c8a11d
Valeriu Iliescu <valeriu.iliescu@resonate-mp4.com>
2026-06-23 10:11:17 +0300

    #40539 - fix tests

--- src/components/ContextMenu/ContextMenu.tsx
+++ src/components/ContextMenu/ContextMenu.tsx
@@ -72,7 +72,7 @@ export class ContextMenu extends React.Component<ContextMenuProps, ContextMenuSt
   }
 
   componentDidUpdate(prevProps: Readonly<ContextMenuProps>, prevState: Readonly<ContextMenuState>, snapshot?: any): void {
-    if (!_.isEqual(this.props.positionToOpen, prevProps.positionToOpen)) {
+    if (this.props.positionToOpen != prevProps.positionToOpen) {
       this.setState({ isAdjusted: false, isOpened: this.props.positionToOpen ? true : false, x: this.props.positionToOpen?.x ?? 0, y: this.props.positionToOpen?.y ?? 0 }, () => {
         this.props.positionToOpen && this.adjustPopup(this.props.positionToOpen.x, this.props.positionToOpen.y)
       });
